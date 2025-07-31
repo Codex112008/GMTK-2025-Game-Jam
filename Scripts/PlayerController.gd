9117d55d4656284f71a2464b2ef608d78ee3c924
epicleemoon <gavinericbutler@gmail.com>
2025-07-31 01:08:47 -0400

    basic character controller done
    COLLECTABLE IS TEST ART SO IF YOU ASK ME WHAT IT IS I AM POINTING YOU TOWARDS THIS COMMIT

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -2,17 +2,34 @@ extends CharacterBody2D
 
 
 @export var speed : float = 300.0
-@export var jump_strength : float = 400.0
+@export var jump_strength : float = 400
+@export var gravity : float = 1600
 
+@export_group("References")
+@export var coyote_timer : Timer
+@export var prejump_timer : Timer
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if velocity.y < 0:
+			velocity += Vector2.DOWN * gravity * delta
+		else:
+			velocity += Vector2.DOWN * gravity * 2 * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -jump_strength
+	if is_on_floor():
+		coyote_timer.start()
+	elif Input.is_action_just_pressed("jump"):
+		prejump_timer.start()
+	
+	# for jump consistency
+	if coyote_timer.time_left != 0 and Input.is_action_just_pressed("jump"):
+		jump()
+		coyote_timer.stop()
+	if prejump_timer.time_left !=0 and is_on_floor():
+		jump()
+		prejump_timer.stop()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,3 +40,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+
+func jump():
+	velocity.y = -jump_strength;
+	# velocity.y = -jump_strength
