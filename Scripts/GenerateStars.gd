10b6657688c7f8f2773656257db04a9aaa9a610a
Codex112008 <codexofcode1120@gmail.com>
2025-08-01 19:24:17 +0800

    Made parralax better?

--- Scripts/GenerateStars.gd
+++ Scripts/GenerateStars.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-@export var star_types : Array = []
+@export var star_types : Array[Texture2D] = []
 
 @export var star_scene : PackedScene
 
@@ -9,30 +9,38 @@ extends CanvasLayer
 @export var background_stars : int = 50
 
 @export_group("References")
-@export var frontmost : ParallaxLayer
-@export var midground : ParallaxLayer
-@export var backmost : ParallaxLayer
+@export var frontmost : Parallax2D
+@export var midground : Parallax2D
+@export var backmost : Parallax2D
 
 var ran : RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	for i in range(0, foreground_stars):
-		var new_star = create_star()
+		var texture = star_types.pick_random()
+		var position = Vector2(ran.randi_range(-440, 1240), ran.randi_range(-360, 360))
+		var new_star = create_star(texture, position)
 		frontmost.add_child(new_star)
+		new_star.position = position
 
 	for i in range(0, midground_stars):
-		var new_star = create_star()
+		var texture = star_types.pick_random()
+		var position = Vector2(ran.randi_range(-440, 1240), ran.randi_range(-360, 360))
+		var new_star = create_star(texture, position)
 		midground.add_child(new_star)
-	
+		new_star.position = position
+		
 	for i in range(0, background_stars):
-		var new_star = create_star()
+		var texture = star_types.pick_random()
+		var position = Vector2(ran.randi_range(-440, 1240), ran.randi_range(-360, 360))
+		var new_star = create_star(texture, position)
 		backmost.add_child(new_star)
+		new_star.position = position
 
 
-func create_star() -> Sprite2D:
+func create_star(texture : Texture2D, position : Vector2) -> Sprite2D:
 	var new_star : Sprite2D = star_scene.instantiate()
-	new_star.texture = star_types.pick_random()
-	new_star.position = Vector2(ran.randi_range(-640, 640), ran.randi_range(-360, 360))
+	new_star.texture = texture
 	new_star.z_index = -1
 
 	return new_star
--- Scripts/MainTileMap.gd
+++ Scripts/MainTileMap.gd
@@ -1,18 +1,18 @@
 extends TileMapLayer
+class_name MainTliemap
 
-@export var tilemap_parent : Node2D
+@export var instantiated_nodes : Node2D
 @export var player : PlayerController
 
-# @export_group("Background")
-# @export var parallax_background : ParallaxBackground
-# @export var parallax_parent : CanvasLayer
+@export_group("Background")
+@export var parallax_background : CanvasLayer
 
 var camera : Camera2D
 var left_tilemap_clone : TileMapLayer
 var right_tilemap_clone : TileMapLayer
 
-# var left_parallax_clone : ParallaxBackground
-# var right_parallax_clone : ParallaxBackground
+var left_parallax_clone : CanvasLayer
+var right_parallax_clone : CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,23 +22,21 @@ func _ready():
 	left_tilemap_clone = duplicate()
 	left_tilemap_clone.position.x -= get_rect_world_pos_x()
 	left_tilemap_clone.set_script(null)
-	tilemap_parent.add_child(left_tilemap_clone)
+	instantiated_nodes.add_child(left_tilemap_clone)
 	
 	right_tilemap_clone = duplicate()
 	right_tilemap_clone.position.x += get_rect_world_pos_x()
 	right_tilemap_clone.set_script(null)
-	tilemap_parent.add_child(right_tilemap_clone)
+	instantiated_nodes.add_child(right_tilemap_clone)
 
 	# duplicate background
-	# left_parallax_clone = parallax_background.duplicate()
-	# left_parallax_clone.offset.x -= get_rect_world_pos_x()
-	# parallax_parent.add_child(left_parallax_clone)
-
-	# right_parallax_clone = parallax_background.duplicate()
-	# right_parallax_clone.offset.x += get_rect_world_pos_x()
-	# parallax_parent.add_child(right_parallax_clone)
-
-	# print(parallax_parent.get_children())
+	#left_parallax_clone = parallax_background.duplicate()
+	#left_parallax_clone.offset.x -= get_rect_world_pos_x()
+	#instantiated_nodes.add_child(left_parallax_clone)
+#
+	#right_parallax_clone = parallax_background.duplicate()
+	#right_parallax_clone.offset.x += get_rect_world_pos_x()
+	#instantiated_nodes.add_child(right_parallax_clone)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
