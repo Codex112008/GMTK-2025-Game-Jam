a25a98f959ac39de4a7fec6a0d86ff25dafb7e8f
epicleemoon <gavinericbutler@gmail.com>
2025-08-01 23:24:54 -0400

    drinks beers

--- Scripts/AnimationManager.gd
+++ Scripts/AnimationManager.gd
@@ -6,23 +6,27 @@ extends AnimatedSprite2D
 var show_cheeks : bool = true
 
 func _process(delta: float) -> void:
-	if player.is_on_floor() && Input.get_axis("left", "right") != 0:
-		play("walk")
-		show_cheeks = true;
-	elif player.is_on_floor():
-		play("idle")
-		show_cheeks = true;
-	elif !player.is_on_floor():
-		if player.spin_timer.time_left != 0:
-			play("spin")
-			show_cheeks = false;
-		else:
-			if player.velocity.y > 0:
-				play("fall")
-				show_cheeks = true;
+	if player.drink_timer.time_left != 0:
+		play("drink")
+		show_cheeks = true
+	else:
+		if player.is_on_floor() && Input.get_axis("left", "right") != 0:
+			play("walk")
+			show_cheeks = true;
+		elif player.is_on_floor():
+			play("idle")
+			show_cheeks = true;
+		elif !player.is_on_floor():
+			if player.spin_timer.time_left != 0:
+				play("spin")
+				show_cheeks = false;
 			else:
-				play("rise")
-				show_cheeks = true;
+				if player.velocity.y > 0:
+					play("fall")
+					show_cheeks = true;
+				else:
+					play("rise")
+					show_cheeks = true;
 	
 	cheeks.visible = show_cheeks && player.nut_count > 0
 
