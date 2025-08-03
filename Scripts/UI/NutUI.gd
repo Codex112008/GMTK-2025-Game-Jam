204c0dfa11123a4f6783dac9eade61490a0b8d91
epicleemoon <gavinericbutler@gmail.com>
2025-08-03 04:55:31 -0400

    screen size changes

--- Scripts/UI/NutUI.gd
+++ Scripts/UI/NutUI.gd
@@ -2,9 +2,10 @@ extends Control
 
 @export var player : PlayerController
 @export var offset : Vector2
+@export var nut_scale : int = 3
 @export var nut_icon : PackedScene
 
-const NUTSIZE : float = 12
+const NUTSIZE : float = 10
 
 var num_nuts = 0
 
@@ -22,7 +23,7 @@ func _process(delta: float) -> void:
 	# make nuts swing
 	for nut in range(0, nuts.size()):
 		var current : TextureRect = nuts[nut]
-		current.position = Vector2(clamp((-player.velocity.x / 50) * nut, -pow(nut, 2), pow(nut, 2)) - player.velocity.x / 50, current.position.y)
+		current.position = Vector2(clamp((-player.velocity.x / 35) * (2 * nut), -pow(2 * nut, 2), pow(2 * nut, 2)) - player.velocity.x / 50, current.position.y)
 
 
 func create_nuts():
@@ -34,8 +35,9 @@ func create_nuts():
 
 	for nut_on in range(0, player.nut_count):
 		var new_icon : TextureRect = nut_icon.instantiate()
+		new_icon.scale = Vector2(nut_scale, nut_scale)
 
-		new_icon.position = Vector2(0, -nut_on * NUTSIZE)
+		new_icon.position = Vector2(0, -nut_on * NUTSIZE * nut_scale)
 
 		nuts.append(new_icon)
 		add_child(new_icon)
